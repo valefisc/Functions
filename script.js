68c98a034c49baa8f3580bd50ba377c17c4fe376
Valerie <valisfis@gmail.com>
2022-02-28 05:34:12 +0100

    first try

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("test");
+
+const firstLetterName = function (name){
+    alert("The name " + name + " starts with the letter " + name[0]);
+}
+
+const divisibleByTwo = function(number){
+    if(number % 2 === 0){
+        console.log("true");
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const largestNum = function(arr){
+    help
+}
+
+
+firstLetterName("Valerie");
+divisibleByTwo(6);
+divisibleByTwo(17);
+largestNum(5, 8, 9, 45);
